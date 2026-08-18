f9d3e33935baf9957c4076d298a0254ed283b05e
Anish Jaiswal <alloozpat87@gmail.com>
2026-08-18 16:24:14 +0545

    using focus Input with useRef

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -3,10 +3,15 @@ import { useRef } from "react";
 
 export default function ClickCounter() {
     let ref = useRef(0);
+    const inputRef = useRef(null)
 
     function handleClick() {
         ref.current = ref.current + 1;
         alert("You Clicked " + ref.current + "times!")
+    }
+
+    function focusInput() {
+        inputRef.current.focus();
     }
 
     return (
@@ -16,6 +21,9 @@ export default function ClickCounter() {
 
             {/* This will not work as ref doen't trigger UI re-render */}
             {/* <h3>Clicked with Ref: {ref.current.valueOf(ClickCounter)}</h3> */}
+
+            <input type="text" ref={inputRef} className="bg-red-300" />
+            <button onClick={focusInput}>Focus</button>
         </div>
 
 
